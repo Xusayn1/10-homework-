662be73facf6682a61d9b504268b9bb21a4bc0c0
Xusayn1 <temurtaragay1336@gmail.com>
2025-04-15 22:10:00 +0500

    Temporary changes before rebase

---
+++ 7 dars/homework.js
@@ -0,0 +1,38 @@
+// 1 task 
+// function sortDrinkByPrice() {
+//     const drinks = [
+//       { name: "lemonade", price: 50 },
+//       { name: "lime", price: 10 },
+//       { name: "Coca-Cola", price: 25 },
+//       { name: "Dena", price: 30 }
+//     ];
+//     return drinks.sort((a, b) => a.price - b.price);
+//   }
+//   console.log(sortDrinkByPrice()); 
+  
+// 2 task 
+// function objectToArray(obj) {
+//     return Object.entries(obj);
+//   }
+  
+//   console.log(objectToArray({
+//     D: 1,
+//     B: 2,
+//     C: 3
+//   }));
+
+//   console.log(objectToArray({
+//     likes: 2,
+//     dislikes: 3,
+//     followers: 10
+//   }));
+
+// task 3 
+
+// function doubleChar(str) {
+//   return str.split('').map(char => char + char).join('');
+// }
+// console.log(doubleChar("String"));      
+// console.log(doubleChar("Hello World!")); 
+// console.log(doubleChar("1234!_ "));      
+
